32c4218931c27b3c88b20be66f0f68366b404872
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-08 19:55:36 +0100

    fix: modify elements position for visual appeal

--- src/components/custom/goalTile.tsx
+++ src/components/custom/goalTile.tsx
@@ -1,8 +1,8 @@
 import {
   CaretUpIcon,
-  DotOutlineIcon,
+  LineVerticalIcon,
   PlusIcon,
-  TrashSimpleIcon,
+  XIcon,
 } from "@phosphor-icons/react/dist/ssr";
 
 interface GoalTileProps {
@@ -27,13 +27,11 @@ export const GoalTile = ({ title, count, isOpen, onToggle }: GoalTileProps) => {
           </span>
         </div>
 
-        <div>
-          <h2 className="font-bold text-sm">
-            {title}{" "}
-            <span className="font-light bg-blue-200 px-1 rounded text-blue-700">
-              {count}
-            </span>
-          </h2>
+        <div className="flex gap-1 items-center">
+          <h2 className="font-bold text-sm line-clamp-1 min-w-0 flex-1">{title}</h2>
+          <span className="font-light text-xs bg-blue-200 px-1 rounded text-blue-700">
+            {count}
+          </span>
         </div>
       </div>
 
@@ -42,13 +40,13 @@ export const GoalTile = ({ title, count, isOpen, onToggle }: GoalTileProps) => {
           onClick={(e) => e.stopPropagation()}
           className="group  border-black px-1.5 py-1 rounded flex items-center gap-0 transition-all duration-300 hover:gap-2 hover:text-red-600"
         >
-          <TrashSimpleIcon size={14} className="shrink-0" />
+          <XIcon weight="bold" size={14} className="shrink-0" />
           <span className="text-sm text-red-600 overflow-hidden whitespace-nowrap max-w-0 transition-all duration-300 group-hover:max-w-32">
             delete
           </span>
         </button>
 
-        <DotOutlineIcon weight="fill" />
+        <LineVerticalIcon className="text-gray-400" />
 
         <button
           onClick={(e) => e.stopPropagation()}
