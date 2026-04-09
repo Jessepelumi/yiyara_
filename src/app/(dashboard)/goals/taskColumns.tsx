c2f467db292f53598f2dca910beef28fbdf49744
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-09 10:24:32 +0100

    feat: add indicator to show task status

--- src/app/(dashboard)/goals/taskColumns.tsx
+++ src/app/(dashboard)/goals/taskColumns.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import {
+  ArrowSquareInIcon,
   ClockCountdownIcon,
   InfoIcon,
-  PenNibIcon,
   RowsIcon,
   ShapesIcon,
 } from "@phosphor-icons/react/dist/ssr";
 import { ColumnDef } from "@tanstack/react-table";
 import { TableRowTitle } from "@/components/custom/tableRowTitle";
 import { Task } from "@/lib/api/types";
+import { StatusIndicator } from "@/components/custom/statusIndicator";
 // import { Checkbox } from "@/components/ui/checkbox";
 // import { Button } from "@/components/ui/button";
 
@@ -39,11 +40,11 @@ export const taskColumns: ColumnDef<Task>[] = [
 
   {
     id: "actions",
-    cell: ({ row }) => {
+    cell: () => {
       return (
-        <div className="flex items-center gap-1">
-          <PenNibIcon />
-          Edit
+        <div className="flex items-center gap-1 text-gray-400">
+          <ArrowSquareInIcon weight="bold" />
+          view
         </div>
       );
     },
@@ -58,13 +59,53 @@ export const taskColumns: ColumnDef<Task>[] = [
     header: () => <TableRowTitle icon={RowsIcon} title="Description" />,
   },
   {
-    accessorKey: "estimation",
-    header: () => (
-      <TableRowTitle icon={ClockCountdownIcon} title="Estimation" />
-    ),
+    accessorKey: "due_date",
+    header: () => <TableRowTitle icon={ClockCountdownIcon} title="Deadline" />,
+    cell: ({ row }) => {
+      const dueDateValue = row.getValue("due_date") as string;
+      if (!dueDateValue) {
+        return <span className="text-gray-400">No set deadline</span>;
+      }
+
+      const today = new Date();
+      const deadline = new Date(dueDateValue);
+
+      // difference in milliseconds
+      const diffInMs = deadline.getTime() - today.getTime();
+
+      // convert calculated difference to days
+      const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
+
+      // deadline passed
+      if (diffInDays < 0) {
+        return (
+          <span className="text-red-600">
+            Overdue by {Math.abs(diffInDays)}d
+          </span>
+        );
+      }
+
+      // deadline date
+      if (diffInDays === 0) {
+        return <span className="text-red-600">Due Today</span>;
+      }
+
+      return <span className="text-green-600">{diffInDays} days left</span>;
+    },
   },
   {
-    accessorKey: "staus",
-    header: () => <TableRowTitle icon={InfoIcon} title="Status" />,
+    accessorKey: "is_completed",
+    header: () => (
+      <div className="flex justify-center items-center">
+        <TableRowTitle icon={InfoIcon} title="Status" />
+      </div>
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-center items-center">
+          <StatusIndicator isCompleted={row.getValue("is_completed")} />
+        </div>
+      );
+    },
   },
 ];
