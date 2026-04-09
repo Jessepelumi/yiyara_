887774066c7317639d5650707d250dd0932d6ad0
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-09 11:44:38 +0100

    fix: modify layout to enforce strict positioning and sizes of children

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,11 +14,11 @@ export default function DashboardLayout({
   const onClose = useCallback(() => setIsMobileSidebarOpen(false), []);
 
   return (
-    <section className="flex h-dvh">
+    <section className="flex h-dvh overflow-hidden">
       <Sidebar />
       <MobileSidebar isOpen={isMobileSidebarOpen} onClose={onClose} />
-      <section className="flex flex-col w-full">
-        <header className="flex justify-between px-3 py-5">
+      <section className="flex flex-col flex-1 min-w-0 h-full">
+        <header className="flex justify-between px-3 py-5 shrink-0">
           <div className="flex items-center">
             <Button
               variant="ghost"
@@ -30,9 +30,14 @@ export default function DashboardLayout({
             <h1>Zimna</h1>
           </div>
 
-          <div>User</div>
+          <div className="flex gap-2 items-center">
+            Jesse
+            <div className="border border-blue-400 p-0.5 rounded-full">
+              <div className="bg-gray-300 w-7 h-7 rounded-full"></div>
+            </div>
+          </div>
         </header>
-        <aside className="flex h-full overflow-hidden px-3 pt-3">
+        <aside className="flex h-full overflow-hidden px-3 pt-3 min-w-0">
           {children}
         </aside>
       </section>
